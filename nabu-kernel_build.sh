839c1809c4b5ec7f363e38992e91e0c9bd717501
chalkin <chalkin@yeah.net>
2024-09-23 02:08:59 +0800

    remove linux-lenovo-q706f LOCALVERSION

--- nabu-kernel_build.sh
+++ nabu-kernel_build.sh
@@ -1,7 +1,7 @@
 git clone https://github.com/silime/linux.git --branch sm8250/6.11-release linux
 cd linux
 curl https://raw.githubusercontent.com/silime/ArchLinux-Packages/refs/heads/main/linux-sm8250/config -o .config
-export LOCALVERSION=-1
+export LOCALVERSION=
 make -j$(nproc) ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- KBUILD_DEBARCH=arm64 KDEB_CHANGELOG_DIST=mobile deb-pkg
 _kernel_version="$(make kernelrelease -s)"
 mkdir ../linux-lenovo-q706f/boot
